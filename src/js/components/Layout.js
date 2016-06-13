8a3cbc431c1efc168337c0892463e59462db5546
dworthen <worthend.derek@gmail.com>
2016-06-12 19:13:46 -0700

    tutorial 5

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -4,12 +4,21 @@ import Footer from './Footer';
 import Header from './Header';
 
 export default class Layout extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      title: "Derek"
+    };
+  }
+
+  changeTitle(title) {
+    this.setState({title});
+  }
 
   render() {
-    const title = "Hello Derek";
     return (  
       <div>
-        <Header title={title} />
+        <Header changeTitle={this.changeTitle.bind(this)} title={this.state.title} />
         <Footer />
       </div>
     );
