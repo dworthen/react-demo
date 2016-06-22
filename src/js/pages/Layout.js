d80f29c9260a26ccbcb94a6e5ea9506133c760d1
dworthen <worthend.derek@gmail.com>
2016-06-21 20:06:33 -0700

    tutorial 8

--- src/js/pages/Layout.js
+++ src/js/pages/Layout.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Link, withRouter } from 'react-router';
+import { Link, withRouter, browserHistory } from 'react-router';
+import _ from 'lodash';
 
 import Footer from '../components/Footer';
 
@@ -12,7 +13,8 @@ export default class Layout extends React.Component {
   navigate(e) {
     e.preventDefault();
     let location = `/settings`;
-    this.props.history.push(location);
+    const { query } = this.props.location;
+    this.props.history.push({pathname: location, query : {...query, newKey: 'test'}});
   }
 
   render() {
@@ -20,7 +22,8 @@ export default class Layout extends React.Component {
       <div>
         <h1>Layout</h1>
         <Link to="/archives" activeClassName="test">Archives</Link>&nbsp;
-        <a href="" onClick={this.navigate.bind(this)}>Settings</a>        
+        <a href="" onClick={this.navigate.bind(this)}>Settings</a>
+        {this.props.location.query.newKey }        
         {this.props.children}
       </div>
     );
